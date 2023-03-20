7d1397e999c2fada35122d323ab1e9f70be01e74
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-03-20 11:21:29 -0300

    repetição com while

---
+++ repeticaoWhile.py
@@ -0,0 +1,10 @@
+numero = 1
+
+while numero != 0:
+
+    numero = int(input('Digite um número: '))
+
+    if numero % 2 == 0:
+        print('Número par!')
+    else:
+        print('Número ímpar!')
