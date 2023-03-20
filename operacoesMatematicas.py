82ba64ffc0a54b1a23a8a0e4555fd0be1063b4a7
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-03-20 10:34:05 -0300

    Aprendendo Python

---
+++ operacoesMatematicas.py
@@ -0,0 +1,19 @@
+# Qual o resultado armazenado na variável operacao_1: 25 ou 17 ? (17)
+operacao_1 = 2 + 3 * 5
+# print(operacao_1)
+print('Resultado em operacao_1 = %s' % (operacao_1))
+
+# Qual o resultado armazenado na variável operacao_2: 25 ou 17 ? (25)
+operacao_2 = (2 + 3) * 5
+# print(operacao_2)
+print('Resultado em operacao_2 = %s' % (operacao_2))
+
+# Qual o resultado armazenado na variável operacao_3: 4 ou 1 ? (1.0)
+operacao_3 = 4 / 2 ** 2
+# print(operacao_3)
+print('Resultado em operacao_3 = %s' % (operacao_3))
+
+# Qual o resultado armazenado na variável operacao_4: 1 ou 5 ? (5)
+operacao_4 = 13 % 3 + 4
+# print(operacao_4)
+print('Resultado em operacao_4 = %s' % (operacao_4))
---
+++ terceiro.py
@@ -0,0 +1,3 @@
+nome = input('Digite um nome: ')
+# print(nome)
+print('Olá %s, bem vindo a disciplina de programação. Parabéns pelo seu primeiro Hello World' % (nome))
